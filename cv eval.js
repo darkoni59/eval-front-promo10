71b0261040307ba41eb2515ddc0fc31aa9ebaae1
darkoni59 <darkoni59610@gmail.com>
2018-12-13 16:42:27 +0100

    eval

---
+++ cv eval.js
@@ -0,0 +1,45 @@
+$('.blue1').click(function ()
+{if ($('.blue1').slideUp('blue1')){
+    $('.blue1').create('div');
+    $('div').html('coucou')
+
+}
+
+
+    $('.blue1').slideDown('blue1')
+
+});
+$('.blue2').click(function () {
+
+    $('.blue2').slideUp('blue2');
+    $('.blue2').slideDown('blue2')
+
+});
+
+
+
+$('.red').click(function () {
+
+    $('.red').slideUp('red');
+    $('.red').slideDown('red')
+});
+$('.orange').click(function () {
+
+    $('.orange').slideUp('orange');
+    $('.orange').slideDown('orange')
+});
+$('.orange2').click(function () {
+
+    $('.orange2').slideUp('orange2');
+    $('.orange2').slideDown('orange2')
+});
+$('.orange1').click(function () {
+
+    $('.orange1').slideUp('orange1');
+    $('.orange1').slideDown('orange1')
+});
+$('.green').click(function () {
+
+    $('.green').slideUp('green');
+    $('.green').slideDown('green')
+});
