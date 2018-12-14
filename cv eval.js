57aef55e09657fc6e049399b03803b6512cf4f2e
darkoni59 <darkoni59610@gmail.com>
2018-12-14 16:18:43 +0100

    eval

--- cv eval.js
+++ cv eval.js
@@ -1,45 +1,91 @@
-$('.blue1').click(function ()
-{if ($('.blue1').slideUp('blue1')){
-    $('.blue1').create('div');
-    $('div').html('coucou')
 
-}
+var tabi=['imageA','imageB'];
+var index=0;
 
 
-    $('.blue1').slideDown('blue1')
+
+
+$('.blue1').hover(function () {
+
+    $(this).show();
+    $(this).html('<h1>expériences</h1><li>Html5</li><li>css++</li><li>javascript</li><li>Jquery</li>');
+
+    $(this).mouseout(function () {
+        $(this).html('<span class="fas fa-book"></span><h2>experience</h2>')
+
+
+    });
+});
+
+$('.blue2').hover(function () {
+
+    $(this).show();
+    $(this).html('<h1>contact</h1><p><strong>mail:bryanbultot@gmail.com</strong></p>'+'<p>n°: 03 27 59 96 71</p>');
+
+$(this).mouseout(function () {
+    $(this).html('<span class="far fa-address-book"></span><h2>contact</h2>')
+
 
 });
-$('.blue2').click(function () {
+});
+$('.red').hover(function () {
 
-    $('.blue2').slideUp('blue2');
-    $('.blue2').slideDown('blue2')
+    $(this).show();
+    $(this).html('<h1>education</h1><li>bac pro hotellerie</li><li>licence Cam</li></span>');
+
+
+    $(this).mouseout(function () {
+        $(this).html('<span class="fas fa-user-graduate"></span><h2>education</h2>');
+
+
+    });
+});
+$('.orange').hover(function () {
+    $(this).show();
+    $(this).html('<h1>social menu</h1> <a href="https://www.linkedin.com/in/bryan-bultot-896505171/"><i class="fab fa-linkedin"></a></span>');
+
+
+    $(this).mouseout(function () {
+        $(this).html('<span class="fas fa-grip-horizontal"></span><h2>social menu</h2>');
+
+
+    });
+});
+$('.orange2').hover(function () {
+
+    $(this).show();
+    $(this).html('<h1>profil</h1><p>je me suis orienté vers la programmation par curiosité</p> <p>suite a cette decouverte j\'en suis tomber amoureux</p>');
+
+
+    $(this).mouseout(function () {
+        $(this).html('<span class="far fa-address-card"></span><h2>profil</h2>');
+
+
+    });
+});
+$('.orange1').hover(function () {
+
+
+    $(this).show();
+    $(this).html('<h1>portfolio</h1><i class="fas fa-dice"></i> <a href="https://darkoni59610.000webhostapp.com/jeuxmemo-bryanbultot/memo.html"> jeux mémo</a><a  href="https://darkoni59610.000webhostapp.com/tri.html">jeu de tri</a>');
+
+
+    $(this).mouseout(function () {
+        $(this).html('<span class="fab fa-dropbox"></span><h2>portfolio</h2>');
+
+
+    });
+});
+$('.green').hover(function () {
+    $(this).show();
+    $(this).html('<h1>blog</h1><a href="https://github.com/darkoni59"><i class="fab fa-github"></a>');
+
+
+    $(this).mouseout(function () {
+        $(this).html('<span class="fas fa-layer-group"></span><h2>blog</h2>');
+
+
+    });
 
 });
-
-
-
-$('.red').click(function () {
-
-    $('.red').slideUp('red');
-    $('.red').slideDown('red')
-});
-$('.orange').click(function () {
-
-    $('.orange').slideUp('orange');
-    $('.orange').slideDown('orange')
-});
-$('.orange2').click(function () {
-
-    $('.orange2').slideUp('orange2');
-    $('.orange2').slideDown('orange2')
-});
-$('.orange1').click(function () {
-
-    $('.orange1').slideUp('orange1');
-    $('.orange1').slideDown('orange1')
-});
-$('.green').click(function () {
-
-    $('.green').slideUp('green');
-    $('.green').slideDown('green')
-});
+$('#carousel').
